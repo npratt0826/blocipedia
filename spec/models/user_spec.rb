bf3e442b08a5d2b843cb649ac4931257b6028f9b
Nick Pratt <nicholasapratt@gmail.com>
2017-08-07 17:02:02 +0000

    some user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,3 +7,24 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:password) }
 end
+
+  describe "attributes" do
+    it "should have name and email attributes" do
+      expect(user).to have_attributes(email: user.email)
+    end
+    it "responds to role" do
+       expect(user).to respond_to(:role)
+     end
+ 
+     it "responds to admin?" do
+       expect(user).to respond_to(:admin?)
+     end
+ 
+     it "responds to member?" do
+       expect(user).to respond_to(:standard?)
+     end
+     
+     it "responds to member?" do
+       expect(user).to respond_to(:premium?)
+     end
+   end
