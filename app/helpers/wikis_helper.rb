bdc8610a08c489f87a7ac5e772e389ab89d8cb35
Nick Pratt <nicholasapratt@gmail.com>
2017-08-22 19:21:19 +0000

    wiki policy resolve

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,2 +1,11 @@
 module WikisHelper
+    
+    def public?
+       wiki.private == nil 
+    end
+    
+    def owner
+       wiki.user_id
+    end
+    
 end
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -28,7 +28,7 @@ class WikiPolicy < ApplicationPolicy
     #   elsif user.role == 'premium'
     #      all_wikis = scope.all
     #      all_wikis.each do |wiki|
-    #       if wiki.private == nil || wiki.owner == user || wiki.collaborators.include?(user)
+    #       if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
     #          wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
     #       end
     #      end
@@ -36,7 +36,7 @@ class WikiPolicy < ApplicationPolicy
     #      all_wikis = scope.all
     #      wikis = []
     #      all_wikis.each do |wiki|
-    #       if wiki.private == nil || wiki.collaborators.include?(user)
+    #       if wiki.public? || wiki.collaborators.include?(user)
     #          wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
     #       end
     #      end
